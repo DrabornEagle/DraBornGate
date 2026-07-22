13d1c3906df810e52c0a2fb4c55ec90884334e22
DrabornEagle <draborneagle@gmail.com>
2026-07-23 00:08:57 +0300

    Set DraBornGate app version to v0.2.0

--- src/config/version.ts
+++ src/config/version.ts
@@ -1,3 +1,3 @@
-export const APP_VERSION = '0.0.2';
+export const APP_VERSION = '0.2.0';
 export const ANDROID_VERSION_CODE = 1;
-export const DEMO_DATA_VERSION = '0.0.2';
+export const DEMO_DATA_VERSION = '0.2.0';
