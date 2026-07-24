df4e2355f9984f68f1bbd3312c9ce81988fc5fe0
DrabornEagle <draborneagle@gmail.com>
2026-07-25 00:25:43 +0300

    Configure Google Maps plugin for standalone Android builds

--- app.config.js
+++ app.config.js
@@ -2,8 +2,16 @@ const base = require('./app.json');
 
 const googleMapsApiKey = process.env.GOOGLE_MAPS_ANDROID_API_KEY || process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY || '';
 const android = { ...base.expo.android };
+const plugins = [...(base.expo.plugins || [])];
+
 if (googleMapsApiKey) {
   android.config = { ...(android.config || {}), googleMaps: { apiKey: googleMapsApiKey } };
+  plugins.push([
+    'react-native-maps',
+    {
+      androidGoogleMapsApiKey: googleMapsApiKey,
+    },
+  ]);
 }
 
 module.exports = {
@@ -11,5 +19,6 @@ module.exports = {
   expo: {
     ...base.expo,
     android,
+    plugins,
   },
 };
