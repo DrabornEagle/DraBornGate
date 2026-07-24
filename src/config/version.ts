db247673c60ed9433c38c041a85d374439a7c91c
DrabornEagle <draborneagle@gmail.com>
2026-07-25 02:59:54 +0300

    Set DraBornGate v0.3.3

--- src/config/version.ts
+++ src/config/version.ts
@@ -1,3 +1,3 @@
-export const APP_VERSION = '0.3.2';
+export const APP_VERSION = '0.3.3';
 export const ANDROID_VERSION_CODE = 1;
-export const DEMO_DATA_VERSION = '0.3.2';
+export const DEMO_DATA_VERSION = '0.3.3';
