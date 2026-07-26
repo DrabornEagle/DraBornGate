df58f7808b997b7e58dfb2d0dd292f08e0142e99
DrabornEagle <draborneagle@gmail.com>
2026-07-26 17:20:02 +0300

    chore: v0.3.9 branding ve zil varlıklarını prebuild öncesi hazırla

--- app.config.js
+++ app.config.js
@@ -1,3 +1,7 @@
+const { ensureBrandingAssets } = require('./scripts/dkd_generate_branding_assets');
 const { ensureNotificationSounds } = require('./scripts/dkd_generate_notification_sounds');
+
+ensureBrandingAssets();
 ensureNotificationSounds();
+
 module.exports = require('./app.json');
