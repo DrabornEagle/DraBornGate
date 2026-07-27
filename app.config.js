04d7995d248c97752b23daf639c8a352fef5bb3a
DrabornEagle <draborneagle@gmail.com>
2026-07-27 12:13:14 +0300

    chore: AdMob kimliklerini ortam değişkeniyle yönet

--- app.config.js
+++ app.config.js
@@ -4,4 +4,13 @@ const { ensureNotificationSounds } = require('./scripts/dkd_generate_notificatio
 ensureBrandingAssets();
 ensureNotificationSounds();
 
-module.exports = require('./app.json');
+const dkdBase = require('./app.json');
+const dkdGoogleAds = dkdBase['react-native-google-mobile-ads'] || {};
+
+module.exports = {
+  ...dkdBase,
+  'react-native-google-mobile-ads': {
+    ...dkdGoogleAds,
+    android_app_id: process.env.ADMOB_ANDROID_APP_ID || dkdGoogleAds.android_app_id || 'ca-app-pub-3940256099942544~3347511713',
+  },
+};
