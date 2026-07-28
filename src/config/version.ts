b7321fae0e910d29b0a84cb30dfffdb010059ac3
DrabornEagle <draborneagle@gmail.com>
2026-07-28 11:37:31 +0300

    chore: merkezi sürümü v0.3.16 yap

--- src/config/version.ts
+++ src/config/version.ts
@@ -1,3 +1,3 @@
-export const APP_VERSION = '0.3.15';
-export const ANDROID_VERSION_CODE = 5;
-export const DEMO_DATA_VERSION = '0.3.15';
+export const APP_VERSION = '0.3.16';
+export const ANDROID_VERSION_CODE = 6;
+export const DEMO_DATA_VERSION = '0.3.16';
