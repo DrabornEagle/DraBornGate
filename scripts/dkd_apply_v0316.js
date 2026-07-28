7ffb024be4bdfb8c1b3fece9b047b0d2a0fb753e
DrabornEagle <draborneagle@gmail.com>
2026-07-28 11:54:38 +0300

    fix: v0.3.16 billing kaynak dönüşümünü düzelt

--- scripts/dkd_apply_v0316.js
+++ scripts/dkd_apply_v0316.js
@@ -1,3 +1,15 @@
 'use strict';
 
-require('./dkd_apply_v0316_safe.js');
+const fs = require('fs');
+const path = require('path');
+
+const dkdSafePath = path.join(__dirname, 'dkd_apply_v0316_safe.js');
+let dkdSafeSource = fs.readFileSync(dkdSafePath, 'utf8');
+const dkdBrokenBillingReplacement = "      `${dkdBillingReturn}\\n}`,\n";
+const dkdFixedBillingReplacement = "      dkdBillingReturn,\n";
+if (dkdSafeSource.includes(dkdBrokenBillingReplacement)) {
+  dkdSafeSource = dkdSafeSource.replace(dkdBrokenBillingReplacement, dkdFixedBillingReplacement);
+  fs.writeFileSync(dkdSafePath, dkdSafeSource, 'utf8');
+}
+
+require(dkdSafePath);
