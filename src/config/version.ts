65568ce21450a548e88e2f7142a4a92518dbba1b
DrabornEagle <draborneagle@gmail.com>
2026-08-02 14:20:49 +0300

    chore: DraBornGate 0.3.17 versionCode 7

--- src/config/version.ts
+++ src/config/version.ts
@@ -1,3 +1,3 @@
-export const APP_VERSION = '0.3.16';
-export const ANDROID_VERSION_CODE = 6;
-export const DEMO_DATA_VERSION = '0.3.16';
+export const APP_VERSION = '0.3.17';
+export const ANDROID_VERSION_CODE = 7;
+export const DEMO_DATA_VERSION = '0.3.17';
